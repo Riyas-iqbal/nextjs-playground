c1a51827423af57154b5b0801fc9f88e30147fef
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-03-22 21:00:55 +0530

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+
+function Header({ title }) {
+  return <h1>
+    <Link href="/posts/first-post">
+      {title ? title : 'Default title'}
+    </Link>
+  </h1>;
+}
+
+export default function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header title="Develop. Preview. Ship. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
